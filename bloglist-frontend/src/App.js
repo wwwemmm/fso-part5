65a8a1a3550b150ff65f3d7a619a1940c69da5cf
Wen Meiqi <896974316@qq.com>
2023-08-17 22:41:08 +0800

    Ex5.5-5.7

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,11 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
+import './index.css'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
+  const [notiInfo, setNotiInfo] = useState([])
+  const [user, setUser] = useState(null)
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -13,39 +22,97 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async  (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setNotiInfo(['Wrong username or password','error'])
+      setTimeout(() => {
+        setNotiInfo([])
+      }, 5000)
+    }
+  }
+
+  const addBlog = async  (blogObject) => {
+    console.log("adding blog ",blogObject.title, blogObject.author)
+    try {
+      const returnedBlog = await blogService.create(blogObject)
+      setBlogs(blogs.concat(returnedBlog))
+      setNotiInfo([`a new blog ${blogObject.title} by ${blogObject.author} added`, 'fulfilled'])
+      blogFormRef.current.toggleVisibility()
+      setTimeout(() => {
+        setNotiInfo([])
+      }, 5000)
+    } catch (exception) {
+      setNotiInfo(['Missing title, author or url', 'error'])
+      setTimeout(() => {
+        setNotiInfo([])
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => {
+    return (
+    <Togglable buttonLabel='login'>
+    <LoginForm
+      username={username}
+      password={password}
+      handleUsernameChange={({ target }) => setUsername(target.value)}
+      handlePasswordChange={({ target }) => setPassword(target.value)}
+      handleSubmit={handleLogin}
+    />
+    </Togglable>
+  )}
+  
+  const blogForm = () => (
+    <Togglable buttonLabel='new blog' ref={blogFormRef}>
+      <BlogForm createBlog={addBlog} />
+    </Togglable>
+  )
+  
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
   }
 
   return (
     <div>
+      {user === null && loginForm()}
+      {user && 
+      <div>
       <h2>blogs</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-      
+      <Notification message={notiInfo[0]} type = {notiInfo[1]}/>
+      <p>
+        <span>{user.name} logged in</span>
+        <button onClick = {handleLogout} >logout</button>
+      </p>
+      {blogForm()}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      </div>
+    }
     </div>
   )
 }
