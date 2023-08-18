d224e2589bad602faa0be4ba7f49d1a6bed02490
Wen Meiqi <896974316@qq.com>
2023-08-18 15:18:39 +0800

    Ex5.14

--- bloglist-frontend/src/components/Blog.test.js
+++ bloglist-frontend/src/components/Blog.test.js
@@ -22,11 +22,11 @@ test('renders content', () => {
     }
   }
   const mockupdateBlog = jest.fn()
-  const deleteBlog = jest.fn()
+  const mockdeleteBlog = jest.fn()
 
   const { container } = render(<Blog blog={blog}
     updateBlog={mockupdateBlog}
-    deleteBlog={deleteBlog}
+    deleteBlog={mockdeleteBlog}
     user={user}
   />)
   const div = container.querySelector('.blog')
@@ -43,3 +43,50 @@ test('renders content', () => {
     'likes'
   )
 })
+
+test('details will be shown affter clicking the controlling button', async() => {
+  const user = {
+    'username':'root',
+    'name':'Super User',
+    'id':'64ddca53179fef78b5747fef'
+  }
+  const blog = {
+    'title':'Blog list tests, step1',
+    'author':'Full Stack Open',
+    'url':'https://fullstackopen.com/en/part5',
+    'likes':0,
+    'user':{
+      'username':'root',
+      'name':'Super User',
+      'id':'64ddca53179fef78b5747fef'
+    }
+  }
+  const mockupdateBlog = jest.fn()
+  const mockdeleteBlog = jest.fn()
+
+  const { container } = render(<Blog blog={blog}
+    updateBlog={mockupdateBlog}
+    deleteBlog={mockdeleteBlog}
+    user={user}
+  />)
+
+  const mockuser = userEvent.setup()
+  const button = screen.getByText('view')
+  await mockuser.click(button)
+
+  const div = container.querySelector('.blog')
+  expect(div).toHaveTextContent(
+    'Blog list tests, step1'
+  )
+  expect(div).toHaveTextContent(
+    'Full Stack Open'
+  )
+  expect(div).toHaveTextContent(
+    'https://fullstackopen.com/en/part5'
+  )
+  expect(div).toHaveTextContent(
+    'likes'
+  )
+})
+
+
